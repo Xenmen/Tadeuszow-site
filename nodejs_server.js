5df75a8e9206eeb4143d35450ac1fa8c26ee021c
Xenmen <dtadeuszow@gmail.com>
2014-07-07 00:31:57 -0700

    Misc - Added todo list
    broken internal link
    font corrections
    setup script improved
    experimental features added to (but not yet enabled in) the run server
    script
    todo list added

--- nodejs_server.js
+++ nodejs_server.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var server = express();
-
 server.use(
   express.static(__dirname + '/site')
 );
-
 var port = 80;
 server.listen(port, function() {
     console.log('Server started...');
 });
+
+/*
+var express = require("express");
+var app = express();
+// serves main page;
+app.get("/", function(req, res) {
+    res.sendfile('index.html')
+});
+app.post("/user/add", function(req, res) {
+    // some server side logic;
+    res.send("OK");
+  });
+// serves all the static files;
+app.get(/^(.+)$/, function(req, res){
+     console.log('static file request : ' + req.params);
+     res.sendfile( "site/" + __dirname + req.params[0]);
+});
+var port = process.env.PORT || 80;
+app.listen(port, function() {
+   console.log("Listening on " + port);
+});
+*/
--- setup.sh
+++ setup.sh
@@ -4,9 +4,12 @@ sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install git nodejs
-#sudo apt-get install git build-essential python-software-properties python g++ make nodejs
 
-npm install -g express-generator
+#sudo apt-get install git build-essential python-software-properties python g++ make nodejs
+#^This probably won't be necessary for anyone who would want to use my site code, and it's certainly not useful to me
+#This script will be cleaned up at some point for ease of use
+
+sudo npm install -g express express-generator
 
 
 
