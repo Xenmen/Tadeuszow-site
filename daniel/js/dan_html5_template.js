77116540056ec807aca138eca3310c2d23ed9f63
Xenmen <dtadeuszow@gmail.com>
2014-08-12 10:40:12 -0700

    Misc link corrections

--- daniel/js/dan_html5_template.js
+++ daniel/js/dan_html5_template.js
@@ -1,10 +1,10 @@
 
 
 function init_template()
-	{
-    document.getElementById("header").innerHTML="<div id='sidebar'><img id='sidebar-image' src='/images/corner/stubborn.png' alt=' ~There was an image here, it's gone now~ ' /><p id='sidebar-text'>Don't be stubborn, be compatible.</p><div class='sidebar-links'><a href='/index.html'>Family Homepage</a><br><a href='/daniel/index.html'>Daniel's Homepage</a><br><a href='/daniel/ambitions.html'>Ambitions</a><br><a href='/daniel/resume.html'>Resume</a><br><a href='/daniel/projects/index.html'>Dan's Projects</a><br><a href='/daniel/html5/index.html'>HTML 5</a><br><a href='/daniel/projects/autoagro.html'>Automated Agriculture</a><br><a href='/daniel/projects/siliquest.html'>Freedom in Computing</a><br><a href='/daniel/projects/thissite.html'>About This Website</a><br></div><br></div>";
-
-    document.getElementById("article").style.width="800px";
-
-    document.getElementById("footer").innerHTML="<br><br><br> <footer> &copy; Copyright 2012 - 2014 Daniel Tadeuszow</footer>";
-	}
+{
+	document.getElementById("header").innerHTML="<div id='sidebar'><img id='sidebar-image' src='/images/corner/stubborn.png' alt=' ~There was an image here, it's gone now~ ' /><p id='sidebar-text'>Don't be stubborn, be compatible.</p><div class='sidebar-links'><a href='/index.html'>Family Homepage</a><br><a href='/daniel/index.html'>Daniel's Homepage</a><br><a href='/daniel/ambitions.html'>Ambitions</a><br><a href='/daniel/resume.html'>Resume</a><br><a href='/daniel/projects/index.html'>Dan's Projects</a><br><a href='/daniel/html5/index.html'>HTML 5</a><br><a href='/daniel/projects/autoagro.html'>Automated Agriculture</a><br><a href='/daniel/projects/siliquest.html'>Freedom in Computing</a><br><a href='/daniel/projects/thissite.html'>About This Website</a><br></div><br></div>";
+	
+	document.getElementById("article").style.width="800px";
+	
+	document.getElementById("footer").innerHTML="<br><br><br> <footer> &copy; Copyright 2012 - 2014 Daniel Tadeuszow</footer>";
+}
